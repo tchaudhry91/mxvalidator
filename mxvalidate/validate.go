afe9c5acb0c156e526475dadc866fcc590459a7a
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2020-06-13 23:53:29 +0530

    Added cors

--- mxvalidate/validate.go
+++ mxvalidate/validate.go
@@ -54,6 +54,18 @@ type DomainResponse struct {
 
 // ValidateMX is a cloud function to validate mx records for domains
 func ValidateMX(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	// Decode request
 	var req ValidateRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
